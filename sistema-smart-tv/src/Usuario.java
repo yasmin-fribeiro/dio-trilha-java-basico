3a076d0ffdf894c1e81d0dfa7987af988b3725e2
yasmin-fribeiro <yasmin.fribeiro@hotmail.com>
2024-05-06 11:47:18 -0300

    Novo execício

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+      
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " +smartTv.volume);
+
+        System.out.println("TV LIGADA? " +smartTv.ligada);
+        System.out.println("Canal Atual : " +smartTv.canal);
+        System.out.println("Volume Atual : " +smartTv.volume);
+
+        smartTv.ligar ();
+
+        System.out.println("Novo status - TV Ligada? " +smartTv.ligada);
+
+        smartTv.desligar ();
+
+        System.out.println("Novo status - TV Ligada? " +smartTv.ligada);
+
+    }
+
+}
