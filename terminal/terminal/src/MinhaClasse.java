1cade42176dea2c72c558339a9449e3e5d5969c3
yasmin-fribeiro <yasmin.fribeiro@hotmail.com>
2024-05-06 12:28:08 -0300

    change class

--- terminal/terminal/src/MinhaClasse.java
+++ terminal/terminal/src/MinhaClasse.java
@@ -1,7 +1,8 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo Terminal");
+        System.out.println("Oi Turma, fui executado pelo Terminal");
     }
+    
 
 }
